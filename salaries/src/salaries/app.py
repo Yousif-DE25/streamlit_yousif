1c1420840c1c3b7e647bbb1a0373dca284434881
67 Yousi <clash.kont.war@gmail.com>
2026-04-23 10:58:26 +0200

    import pages

---
+++ salaries/src/salaries/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+pages = [
+    st.Page("pages/home.py", title = "Home"),
+    st.Page("pages/raw_data.py", title = "Raw Data"),
+    st.Page("pages/dashboard.py", title = "Dashboard"),
+]
+
+pg = st.navigation(pages)
+
+pg.run()
