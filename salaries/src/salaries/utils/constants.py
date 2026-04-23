be7e3678f21a695ced8d53402104cb6a35e45102
67 Yousi <clash.kont.war@gmail.com>
2026-04-23 10:29:19 +0200

    import constans

---
+++ salaries/src/salaries/utils/constants.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parent[1]
+
+ASSETS_PATH = BASE_PATH / "assets"
+COMPONENTS_PATH = BASE_PATH / "components"
+
+DATA_PATH = ASSETS_PATH / "data"
+STYLES_PATH = ASSETS_PATH / "styles"
+MARKDOWN_PATH = ASSETS_PATH / "markdown"
+IMAGES_PATH = ASSETS_PATH / "images"
+
